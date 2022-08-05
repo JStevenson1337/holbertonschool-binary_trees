eac2c01d33ee346abc2c1e1f532e6060232088c1
Jeremy Stevenson <jeremy@parallax.jstevenson1337.github.beta.tailscale.net>
2022-08-04 19:21:09 -0500

    add: 0, 1, 2

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child
+ * @parent: Pointer to the parent node
+ * @value: value to store in the new node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+    while (parent != NULL)
+    {
+        if (parent->left != NULL)
+        {
+            parent = parent->left;
+        }
+        else
+        {
+            node = binary_tree_node(parent, value);
+            if (node == NULL)
+                return (NULL);
+            parent->left = node;
+            return (node);
+        }
+    }
+    return (NULL);
+    }
+
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+    while (parent != NULL)
+    {
+        if (parent->right != NULL)
+        {
+            parent = parent->right;
+        }
+        else
+        {
+            node = binary_tree_node(parent, value);
+            if (node == NULL)
+                return (NULL);
+            parent->right = node;
+            return (node);
+        }
+    }
+    return (NULL);
+}
