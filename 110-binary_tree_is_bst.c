32877ecb83bc057138e5d19a882b73cb81c7d6df
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2022-08-10 21:24:35 -0500

    Fix: 110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -7,31 +7,21 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree || tree == NULL)
-		return 0;
-
-	while (tree->left != NULL)
-		tree = tree->left;
-	while (tree->right != NULL)
-		tree = tree->right;
-	return (binary_tree_is_bst_rec(tree, INT_MIN, INT_MAX));
+	if (tree == NULL)
+		return (1);
+	if (tree->left != NULL)
+	{
+		if (tree->n < tree->left->n)
+			return (0);
+		if (!binary_tree_is_bst(tree->left))
+			return (0);
+	}
+	if (tree->right != NULL)
+	{
+		if (tree->n > tree->right->n)
+			return (0);
+		if (!binary_tree_is_bst(tree->right))
+			return (0);
+	}
+	return (1);
 }
-
-/**
- * binary_tree_is_bst_rec - checks if a binary tree is a valid Binary Search Tree
- * 
- * @tree: pointer to the root node of the tree 
- * @min: minimum value of the BST
- * @max: maximum value of the BST
- * @return: return 1 if tree is a valid BST, and 0 otherwise
- */
-int binary_tree_is_bst_rec(const binary_tree_t *tree, int min, int max)
-{
-	if (!tree || tree == NULL)
-		return 1;
-	if (tree->n < min || tree->n > max)
-		return 0;
-	return (binary_tree_is_bst_rec(tree->left, min, tree->n - 1) &&
-		binary_tree_is_bst_rec(tree->right, tree->n + 1, max));
-}
-
