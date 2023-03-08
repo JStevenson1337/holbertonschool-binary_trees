b9630907e6314b1c7f972f6aa56986f4cb462579
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2023-03-08 03:45:52 -0600

    add rotate left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,14 +6,23 @@
   *
   */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
-  {
+{
+	binary_tree_t *node;
+	int x = 0;
 
-   if (!tree)
-     {
-       return (-1);
-     }
-
-
-    return (0);
-
-  }
+	if (!tree)
+	{
+		return (NULL);
+	}
+	/* root of parent == NULL */
+	if (node->next != '\0')
+	{
+		if (node->left->left == NULL)
+		{
+			node->right = node->left->left;
+			printf("%i", node -> n);
+		}
+		
+	}
+	return (tree);
+}
